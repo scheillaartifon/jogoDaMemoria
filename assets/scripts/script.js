b1c33cee5581c3bdb0120e79f0383d51ccf6cd95
Scheilla <scheillaartifon9805@hotmail.com>
2023-01-03 10:37:48 -0300

    Montando o tabuleiro e deixando as cartas do tabuleiro menores

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,7 @@
 const FRONT = "card_front"
 const BACK = "card_back"
+const CARD = "card"
+const ICON = "icon"
 
 let techs = [
     'bootstrap',
@@ -21,10 +23,49 @@ startGame();
 function startGame(){
     cards = createCardsFromTechs(techs)
     shuffleCards(cards);
+
+    initializeCards(cards);
+}
+
+function initializeCards(){
+    let gameBoard = document.getElementById("gameBoard")
+    
+    cards.forEach(card => {
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+
+        createCardContent(card, cardElement);
+
+        cardElement.addEventListener('click', flipCard)
+        gameBoard.appendChild(cardElement);
+
+    })
+}
+
+function createCardContent(card, cardElement){
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+}
+
+function createCardFace(face, card, element){
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+
+    if(face === FRONT){
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = "./assets/images/" + card.icon + ".png";
+        cardElementFace.appendChild(iconElement);
+    }else{
+        cardElementFace.innerHTML = "&lt/&gt"
+    }
+    element.appendChild(cardElementFace);
 }
 
 function shuffleCards(cards){
-    let currentIndex = cards.lenght;
+    let currentIndex = cards.length;
     let randomIndex = 0;
 
     while(currentIndex !== 0){
@@ -38,9 +79,9 @@ function shuffleCards(cards){
 function createCardsFromTechs(techs){
     let cards = [];
 
-    for(let tech of techs){
+    techs.forEach((tech) => {
         cards.push(createPairFromTech(tech));
-    }
+    })
 
     return cards.flatMap(pair => pair)
 }
@@ -60,4 +101,8 @@ function createPairFromTech(tech){
 
 function createIdWithTech(tech){
     return tech + parseInt(Math.random() * 1000)
+}
+
+function flipCard(){
+    this.classList.add("flip");
 }
