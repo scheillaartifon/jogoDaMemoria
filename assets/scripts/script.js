8db5f0c4a1f72327a01645834f869fc58d1d64ea
Scheilla <scheillaartifon9805@hotmail.com>
2023-01-03 11:30:55 -0300

    Checando se forma par e corrigindo bugs

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -47,5 +47,21 @@ function createCardFace(face, card, element){
 }
 
 function flipCard(){
-    this.classList.add("flip");
+    if(game.setCard(this.id)){
+        this.classList.add("flip");
+
+        if(game.secondCard){
+            if(game.checkMacth()){
+                game.clearCards();
+            }else{
+                setTimeout(() => {
+                    let firstCardView = document.getElementById(game.firstCard.id);
+                    let secondCardView = document.getElementById(game.secondCard.id);
+                    firstCardView.classList.remove('flip');
+                    secondCardView.classList.remove('flip');
+                    game.unflipCards();
+                }, 1000);
+            }
+        };
+        }
 }
